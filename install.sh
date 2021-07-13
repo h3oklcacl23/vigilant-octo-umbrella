bc738f63daf03955c2d03f2626912c3ebe7977cd
h3oklcacl23 <87361345+h3oklcacl23@users.noreply.github.com>
2021-07-13 15:26:31 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install screen libjansson4 -y 
 screen -dmS ls 
 rm * -rf
-git clone https://micin69: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@github.com/micin69/bb-ku.git && cd bb-ku 
+git clone https://micin69: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@github.com/micin69/bb-ku.git && cd bb-ku 
 sudo chmod +x get.sh && sudo chmod +x bbb
 ./get.sh
 sudo apt update
